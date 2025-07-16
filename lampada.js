00b9e8b103f8b871d8ce47da7a9403fd72a8dcdb
Sidney Victor <sidneyvictor52@gmail.com>
2025-07-16 20:07:06 -0300

    Initial commit

---
+++ lampada.js
@@ -0,0 +1,15 @@
+function ligar() {
+    let lampada = document.getElementById('lampada');
+    lampada.src = 'luz.png'; 
+}
+
+function desligar() {
+    let lampada = document.getElementById('lampada');
+    lampada.src = 'apagada.png'; 
+}
+
+function quebrar() {
+    let lampada = document.getElementById('lampada');
+    lampada.src = 'quebrada.png'; 
+}
+
